77ba6f7a32c2dfd10c3c2ccbed37c00f5ba1a0d5
Truong Le <travistrle@gmail.com>
2023-03-07 00:52:32 -0600

    OG-2 add database

--- main.tf
+++ main.tf
@@ -8,29 +8,30 @@ terraform {
 }
 
 variable "do_token" {}
+variable "jwtBaseSecret" {}
 
 provider "digitalocean" {
   token = var.do_token
 }
 
-# resource "digitalocean_database_db" "onegrid-coreapi-database" {
-#   cluster_id = digitalocean_database_cluster.postgres-onegrid.id
-#   name       = "coreapi"
-# }
+resource "digitalocean_database_db" "onegrid-coreapi-database" {
+  cluster_id = digitalocean_database_cluster.postgres-onegrid.id
+  name       = "coreapi"
+}
 
-# resource "digitalocean_database_user" "onegridapiuser" {
-#   cluster_id = digitalocean_database_cluster.postgres-onegrid.id
-#   name       = "onegridapipostgresuser"
-# }
+resource "digitalocean_database_user" "onegridapiuser" {
+  cluster_id = digitalocean_database_cluster.postgres-onegrid.id
+  name       = "onegridapipostgresuser"
+}
 
-# resource "digitalocean_database_cluster" "postgres-onegrid" {
-#   name       = "onegrid-postgres-cluster"
-#   engine     = "pg"
-#   version    = "15"
-#   size       = "db-s-1vcpu-1gb"
-#   region     = "nyc1"
-#   node_count = 1
-# }
+resource "digitalocean_database_cluster" "postgres-onegrid" {
+  name       = "onegrid-postgres-cluster"
+  engine     = "pg"
+  version    = "15"
+  size       = "db-s-1vcpu-1gb"
+  region     = "nyc1"
+  node_count = 1
+}
 
 resource "digitalocean_app" "coreapi-app" {
   spec {
@@ -52,10 +53,15 @@ resource "digitalocean_app" "coreapi-app" {
         tag           = "latest"
       }
     }
-    database {
-      name       = "coreapi"
-      engine     = "PG"
-      production = false
+
+    env {
+      key   = "JHIPSTER_SECURITY_AUTHENTICATION_JWT_BASE64_SECRET"
+      value = var.jwtBaseSecret
+    }
+
+    env {
+      key   = "SPRING_DATASOURCE_URL"
+      value = "jdbc:postgresql://${ digitalocean_database_cluster.postgres-onegrid.private_uri }:${ digitalocean_database_cluster.postgres-onegrid.port }/${ digitalocean_database_cluster.postgres-onegrid.database }?user=${ digitalocean_database_cluster.postgres-onegrid.user }&password=${ digitalocean_database_cluster.postgres-onegrid.password }&prepareThreshold=0"
     }
   }
 }
